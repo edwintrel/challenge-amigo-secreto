228376f732a0d90a110a1b292270b84d250de324
Edwin Trejo <eg.trejo117@gmail.com>
2025-08-01 02:53:37 -0600

    Implementacion de función para actualizar lista de amigos

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ let amigos = [];
 
 function agregarAmigo() {
     let nombres = document.getElementById('amigo').value;
+    let listaNombres = document.querySelector('#listaAmigos');
+    listaNombres.innerHTML = "";
     if (nombres == '') {
         alert('Introduce un nombre profavor')
     } else {
         amigos.push(nombres);
         console.log(amigos);
+        addLista();
     }
     limpiarCaja();
 }
@@ -16,4 +19,12 @@ function limpiarCaja() {
     let valorCaja = document.querySelector('#amigo').value = '';
 }
 
-
+function addLista() {
+  var contenido="";
+  for (i = 0; i < amigos.length; i++) {
+    var nuevoElemento = document.createElement("li");           // crea un nuevo elemento
+    var nuevoContenido = document.createTextNode(amigos[i]);    // y añade contenido
+    nuevoElemento.appendChild(nuevoContenido);                  //añade el contenido al elemento
+    document.querySelector("#listaAmigos").appendChild(nuevoElemento);
+  }
+}
