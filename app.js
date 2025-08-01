e313269febfbb625c9bd50d820e8576cabb992cc
Edwin Trejo <eg.trejo117@gmail.com>
2025-08-01 01:49:15 -0600

    modificando app.js para guardar datos en un array

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let nombres = document.getElementById('amigo').value;
+    if (nombres == '') {
+        alert('Introduce un nombre profavor')
+    } else {
+        amigos.push(nombres);
+        console.log(amigos);
+    }
+    limpiarCaja();
+}
+
+function limpiarCaja() {
+    let valorCaja = document.querySelector('#amigo').value = '';
+}
+
+
